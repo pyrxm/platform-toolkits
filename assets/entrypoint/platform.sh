4b3d61d3d6062a07a3160ae0d39693265dd63e67
Xan Manning <xan.manning+github-work@gmail.com>
2026-05-03 13:34:12 +0100

    refactor: entrypoint

--- assets/entrypoint/platform.sh
+++ assets/entrypoint/platform.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-PTK_CONFIG="${PLATFORM_TOOLKIT_CONFIG_PATH:-/tmp}"
-PTK_INIT="${HOME}/.ptk-init"
-
-PTK_CONFIG_FILES=(
-    "devbox.json"
-    "devbox.lock"
-    "mise.toml"
-    ".tool-versions"
-)
-
-for PTK_CFILE in "${PTK_CONFIG_FILES[@]}" ; do
-    if [ -f "${PTK_CONFIG}/${PTK_CFILE}" ] && [ ! -e "${HOME}/${PTK_CFILE}" ] ; then
-        ln -s "${PTK_CONFIG}/${PTK_CFILE}" "${HOME}/${PTK_CFILE}"
-    fi
-done
+export PTK_INIT="${HOME}/.ptk-init"
 
 if [ -n "$(command -v devbox)" ] ; then
     export PATH="${HOME}/.nix-profile/bin:${HOME}/.devbox/nix/profile/default/bin:${PATH}"
@@ -24,6 +10,6 @@ fi
 
 [ -n "$(command -v mise)" ] && eval "$(mise activate --shims)"
 
-[ -f "${PTK_INIT}" ] && task build-env && touch "${PTK_INIT}"
+[ ! -f "${PTK_INIT}" ] && task build-env
 
 eval "$@"
